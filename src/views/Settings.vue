033d885c0d63b8eb559de0d62cc0a9a6c57e0af6
miratech <welekwemiracle63@gmail.com>
2023-05-03 16:15:26 +0100

    Edited the Settings Page

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,11 +1,26 @@
 <template>
     <div>
       <h1>Settings</h1>
+
+      <b-container>
+        <form>
+          <input v-model="name" type="text" class="form-control mb-3">
+          <input v-model="password" type="text" class="form-control mb-3">
+          <input v-model="confPassword" type="text" class="form-control mb-3">
+        </form>
+      </b-container>
     </div>
 </template>
 
 <script>
     export default {
+      data(){
+        return{
+          name:'',
+          password:'',
+          confPassword:''
+        }
+      }
         
     }
 </script>
